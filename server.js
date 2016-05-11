d2f782b400cbf346f0db6d812d4de0a830a61467
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-05-11 03:47:07 -0700

    finished but no formidable package included

--- server.js
+++ server.js
@@ -1,38 +1,14 @@
-var http = require ('http');
+
+var http = require('http');
 var url = require('url');
 
-function start(route){
-  function onRequrest(request,response){
+function start(route, handle){
+  function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
-    console.log('Request for '+ pathname + ' received.' );
-    route(pathname);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
-    response.end();
+    console.log('Request for '+ pathname + 'received.');
+    route(handle, pathname, response, request);
   }
-  http.createServer(onRequrest).listen(8888);
+  http.createServer(onRequest).listen(8888);
   console.log('Server has started.');
 }
 exports.start = start;
-// var http = requrie('http');
-// var server = http.createServer();
-// server.listen(8888);
-
-// function say(word){
-//   console.log(word);
-// }
-//
-// function execute(someFunction, value){
-//   someFunction(value);
-// }
-// execute(say, 'Hello');
-//
-// function execute(someFunction, value){
-//   someFunction(value);
-// }
-// execute(function(word){ console.log(word);},'Hello');
-//
-// var result = database.query('SELECT * FROM hugetable', function(rows){
-//   var result = rows;
-// });
-// console.log('Hello World');
